efa580cf9eb7e0816a30dbc75fd8b0dcf234addb
audiobird <cantrellhepner@gmail.com>
2025-04-01 14:20:46 -0700

    Delete build files

--- build/plugin-sdk/metamodule-plugin-libc/CMakeFiles/metamodule-plugin-libc.dir/dso_handle.c.obj.d
+++
@@ -1,2 +0,0 @@
-plugin-sdk/metamodule-plugin-libc/CMakeFiles/metamodule-plugin-libc.dir/dso_handle.c.obj: \
- /Users/ross/documents/metamodule-plugin-sdk-2.0-dev/metamodule-plugin-libc/dso_handle.c
--- build/plugin-sdk/metamodule-plugin-libc/CMakeFiles/metamodule-plugin-libc.dir/libstdc++-v3/libsupc++/atomicity.cc.obj.d
+++
@@ -1,2 +0,0 @@
-plugin-sdk/metamodule-plugin-libc/CMakeFiles/metamodule-plugin-libc.dir/libstdc++-v3/libsupc++/atomicity.cc.obj: \
- /Users/ross/documents/metamodule-plugin-sdk-2.0-dev/metamodule-plugin-libc/libstdc++-v3/libsupc++/atomicity.cc
